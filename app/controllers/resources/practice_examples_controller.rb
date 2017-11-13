6e4f0d5b1698de5008926d9ae856493da0766b75
James Jefferies <james@shedcode.co.uk>
2017-11-13 14:06:51 +0000

    WIP for pracitce examples and tools

---
+++ app/controllers/resources/practice_examples_controller.rb
@@ -0,0 +1,75 @@
+class PracticeExamplesController < ApplicationController
+  before_action :set_practice_example, only: [:show, :edit, :update, :destroy]
+
+  # GET /practice_examples
+  def index
+    @practice_examples = PracticeExample.all
+  end
+
+  # GET /practice_examples/1
+  def show
+  end
+
+  # GET /practice_examples/new
+  def new
+    @practice_example = PracticeExample.new
+    populate_practice_example_chunks
+  end
+
+  # GET /practice_examples/1/edit
+  def edit
+    populate_practice_example_chunks
+  end
+
+  # POST /practice_examples
+  def create
+    @practice_example = PracticeExample.new(practice_example_params)
+
+    if @practice_example.save
+      redirect_to [:resources, @practice_example], notice: 'Practice example was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /practice_examples/1
+  def update
+    if @practice_example.update(practice_example_params)
+      redirect_to [:resources, @practice_example], notice: 'Practice example was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /practice_examples/1
+  def destroy
+    @practice_example.destroy
+    redirect_to resources_url, notice: 'Practice example was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_practice_example
+      @practice_example = PracticeExample.find_by_slug(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def practice_example_params
+      params.require(:practice_example)
+        .permit(:title, :intro,
+          what_was_the_problem_chunk_attributes: [:title, :intro, :content, :after],
+          how_did_we_approach_it_chunk_attributes: [:title, :intro, :content, :after],
+          what_did_we_do_chunk_attributes: [:title, :intro, :content, :after],
+          did_it_work_chunk_attributes: [:title, :intro, :content, :after],
+          recommendations_chunk_attributes: [:title, :intro, :content, :after],
+        )
+    end
+
+    def populate_practice_example_chunks
+      @practice_example.build_what_was_the_problem_chunk    if @practice_example.what_was_the_problem_chunk.nil?
+      @practice_example.builf_how_did_we_approach_it_chunk  if @practice_example.how_did_we_approach_it_chunk.nil?
+      @practice_example.build_what_did_we_do_chunk          if @practice_example.what_did_we_do_chunk.nil?
+      @practice_example.build_did_it_work_chunk             if @practice_example.did_it_work_chunk.nil?
+      @practice_example.build_recommendations_chunk         if @practice_example.recommendations_chunk.nil?
+    end
+end
---
+++ app/controllers/resources/tools_controller.rb
@@ -0,0 +1,87 @@
+class ToolsController < ApplicationController
+  before_action :set_tool, only: [:show, :edit, :update, :destroy]
+
+  # GET /tools
+  def index
+    @tools = Tool.all
+  end
+
+  # GET /tools/1
+  def show
+  end
+
+  # GET /tools/new
+  def new
+    @tool = Tool.new
+    populate_tool_chunks
+  end
+
+  # GET /tools/1/edit
+  def edit
+    populate_tool_chunks
+  end
+
+  # POST /tools
+  def create
+    @tool = Tool.new(tool_params)
+
+    if @tool.save
+      redirect_to [:resources, @tool], notice: 'Tool was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /tools/1
+  def update
+    if @tool.update(tool_params)
+      redirect_to [:resources, @tool], notice: 'Tool was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /tools/1
+  def destroy
+    @tool.destroy
+    redirect_to resources_url, notice: 'Tool was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_tool
+      @tool = Tool.find_by_slug(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def tool_params
+      params.require(:tool)
+        .permit(:title, :intro,
+          explanation_chunk_attributes: [:title, :intro, :content, :after],
+          where_it_s_from_chunk_attributes: [:title, :intro, :content, :after],
+          numbered_paragraph_chunk_attributes: [:title, :intro, :content, :after],
+          attachments_chunk_attributes: [:title, :intro, :after,
+          external_resources_attributes: [:title, :url]],
+        )
+    end
+
+    def populate_tool_chunks
+      @tool.build_explanation_chunk         if @tool.explanation_chunk.nil?
+      @tool.build_where_it_s_from_chunk     if @tool.where_it_s_from_chunk.nil?
+      @tool.build_numbered_paragraph_chunk  if @tool.numbered_paragraph_chunk.nil?
+
+      if @tool.attachments_chunk.nil?
+        @tool.build_attachments_chunk
+        MAXIMUM_NUMBER_OF_EXTERNAL_RESOURCES.times do
+          @tool.attachments_chunk.external_resources.build
+        end
+      else
+        number_of_external_resources = @tool.attachments_chunk.external_resources.count
+        if number_of_external_resources < MAXIMUM_NUMBER_OF_EXTERNAL_RESOURCES
+          (MAXIMUM_NUMBER_OF_EXTERNAL_RESOURCES - number_of_external_resources).times do
+             @tool.attachments_chunk.external_resources.build
+          end
+        end
+      end
+    end
+end
