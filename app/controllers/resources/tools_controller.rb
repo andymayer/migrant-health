dafb3273ef6e9b03a66481fdaf66383395cfc425
James Jefferies <james@shedcode.co.uk>
2017-11-15 10:29:51 +0000

    Namespace resources

--- app/controllers/resources/tools_controller.rb
+++ app/controllers/resources/tools_controller.rb
@@ -1,56 +1,58 @@
-class Resources::ToolsController < ApplicationController
-  before_action :set_tool, only: [:show, :edit, :update, :destroy]
+module Resources
+  class ToolsController < ApplicationController
+    before_action :set_tool, only: [:show, :edit, :update, :destroy]
 
-  MAXIMUM_NUMBER_OF_EXTERNAL_RESOURCES = 6
+    MAXIMUM_NUMBER_OF_EXTERNAL_RESOURCES = 6
 
-  # GET /tools
-  def index
-    @resource_type = 'Tool'
-    @tools = Tool.all
-  end
-
-  # GET /tools/1
-  def show
-  end
-
-  # GET /tools/new
-  def new
-    @tool = Tool.new
-    populate_tool_chunks
-  end
-
-  # GET /tools/1/edit
-  def edit
-    populate_tool_chunks
-  end
-
-  # POST /tools
-  def create
-    @tool = Tool.new(tool_params)
-
-    if @tool.save
-      redirect_to [:resources, @tool], notice: 'Tool was successfully created.'
-    else
-      render :new
+    # GET /tools
+    def index
+      @resource_type = 'Tool'
+      @tools = Tool.all
     end
-  end
 
-  # PATCH/PUT /tools/1
-  def update
-    if @tool.update(tool_params)
-      redirect_to [:resources, @tool], notice: 'Tool was successfully updated.'
-    else
-      render :edit
+    # GET /tools/1
+    def show
     end
-  end
 
-  # DELETE /tools/1
-  def destroy
-    @tool.destroy
-    redirect_to resources_url, notice: 'Tool was successfully destroyed.'
-  end
+    # GET /tools/new
+    def new
+      @tool = Tool.new
+      populate_tool_chunks
+    end
 
-  private
+    # GET /tools/1/edit
+    def edit
+      populate_tool_chunks
+    end
+
+    # POST /tools
+    def create
+      @tool = Tool.new(tool_params)
+
+      if @tool.save
+        redirect_to @tool, notice: 'Tool was successfully created.'
+      else
+        render :new
+      end
+    end
+
+    # PATCH/PUT /tools/1
+    def update
+      if @tool.update(tool_params)
+        redirect_to @tool, notice: 'Tool was successfully updated.'
+      else
+        render :edit
+      end
+    end
+
+    # DELETE /tools/1
+    def destroy
+      @tool.destroy
+      redirect_to resources_url, notice: 'Tool was successfully destroyed.'
+    end
+
+    private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_tool
       @tool = Tool.find_by_slug(params[:id])
@@ -58,7 +60,7 @@ class Resources::ToolsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def tool_params
-      params.require(:tool)
+      params.require(:resources_tool)
         .permit(:title, :intro,
           explanation_chunk_attributes: [:title, :intro, :content, :after],
           where_it_s_from_chunk_attributes: [:title, :intro, :content, :after],
@@ -87,4 +89,5 @@ class Resources::ToolsController < ApplicationController
         end
       end
     end
+  end
 end
