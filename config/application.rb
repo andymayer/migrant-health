128625f94bb5d804d4b330917530d323d9a94f61
James Jefferies <james@shedcode.co.uk>
2017-11-20 11:14:38 +0000

    Take pundit out of here thought

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,6 @@ module MigrantHealth
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    config.action_dispatch.rescue_responses["Pundit::NotAuthorizedError"] = :forbidden
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
