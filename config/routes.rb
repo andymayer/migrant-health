d2386cca982ce6384d7cd5e4bd1dcbddfa661756
James Jefferies <james@shedcode.co.uk>
2017-11-23 14:35:18 +0000

    Add users view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
   devise_for :users
+
   root 'static_pages#home'
 
-  get 'users/:id', to: 'users#show', as: :user_profile
+  get 'users/:id', to: 'users#show',  as: :user_profile
+  get 'users',     to: 'users#index', as: :users
 
   resources :resources, only: :index
 
