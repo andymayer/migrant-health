f20f1df2859ff384d7de3178fa02aa30e29ea58d
Andy Mayer <andy@yoomee.com>
2017-11-30 19:07:08 +0000

    Add icon for country

--- app/models/resources/country.rb
+++ app/models/resources/country.rb
@@ -24,11 +24,11 @@
 module Resources
   class Country < Resource
     belongs_to :resources_chunk,             optional: true, class_name: 'FurtherInformationChunk', foreign_key: 'further_information_chunk_id'
-    
+
     accepts_nested_attributes_for :resources_chunk,              reject_if: :all_blank
 
     def icon_file_name
-      'icon-fact-sheet.svg'
+      'icon-country.svg'
     end
 
     def icon_alt_text
