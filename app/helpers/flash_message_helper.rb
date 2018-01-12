b2a633e9f30227bb66b0b29b0de170b706f65ea8
James Jefferies <james@shedcode.co.uk>
2018-01-12 10:04:05 +0000

    Don't show timed out flash

--- app/helpers/flash_message_helper.rb
+++ app/helpers/flash_message_helper.rb
@@ -2,6 +2,7 @@ module FlashMessageHelper
 
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
+      next unless message.is_a? String
       concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do
               concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
               concat message
