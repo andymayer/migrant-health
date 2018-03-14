b312f4192c3f05c566153400ae0bda5357ec0e6e
Andy Mayer <andy@yoomee.com>
2018-03-14 19:04:48 +0000

    Force sign in to home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -51,6 +51,10 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
   def not_found
     render :template => "errors/not_found", :status => 404
   end
