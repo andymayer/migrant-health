a4bf1957463dfc64fd27ac0c0a2ff0eeaf894bd3
Andy Mayer <andy@yoomee.com>
2018-05-04 18:30:52 +0100

    Better error messages

--- app/helpers/flash_message_helper.rb
+++ app/helpers/flash_message_helper.rb
@@ -2,6 +2,7 @@ module FlashMessageHelper
 
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
+      message = replace_text(message)
       next unless message.is_a? String
       concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do
               concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
@@ -15,6 +16,15 @@ module FlashMessageHelper
 
   def bootstrap_class_for flash_type
     { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }.stringify_keys[flash_type.to_s] || flash_type.to_s
+  end
+
+  def replace_text(message)
+    case message
+    when "You are not authorised to new this question"
+      return "You need to be <a href='/users/sign_in'>registered and logged in</a> to ask a question.".html_safe
+    else
+      return message
+    end
   end
 
 end
